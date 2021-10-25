b1ec174ef5f1d5a469bc022973b7c226ad3d2f1e
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-25 03:35:59 -0600

    adding function to check for columns of three. clear those three cells if the colors of those cells match in a column. set a timer to call this function every 100ms

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ const candyColors = [
 
 const App = () => {
   const [currentColorArrangement,setCurrentColorArrangement] = useState([])
+
+  const checkForColumnsOfThree = () => {
+    for(let i = 0; i < 47; i++) {
+      const columnOfThree = [i , i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+      if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
   
   const createBoard = () => {
     const randomColorArrangement = []
@@ -24,6 +34,12 @@ const App = () => {
   useEffect(() => {
     createBoard()
   }, [])
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnsOfThree()
+    }, 100)
+    return() => clearInterval(timer)
+  }, [checkForColumnsOfThree])
   return (
     <div className="app">
       <div className="game">
