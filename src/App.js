9c4be224be3087da81562b67ffcb07159b203352
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-25 03:54:36 -0600

    fixed columnOfThree array to correctly detect column of three matching colors

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ const candyColors = [
 const App = () => {
   const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
-  const checkForColumnsOfThree = () => {
+  const checkForColumnOfThree = () => {
     for(let i = 0; i < 47; i++) {
-      const columnOfThree = [i , i + width * 2]
+      const columnOfThree = [i, i + width, i + width * 2]
       const decidedColor = currentColorArrangement[i]
       if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
         columnOfThree.forEach(square => currentColorArrangement[square] = '')
@@ -36,16 +36,16 @@ const App = () => {
   }, [])
   useEffect(() => {
     const timer = setInterval(() => {
-      checkForColumnsOfThree()
+      checkForColumnOfThree()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return() => clearInterval(timer)
-  }, [checkForColumnsOfThree])
+  }, [checkForColumnOfThree, currentColorArrangement])
   return (
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((candyColor,index) => (
-          <img key={index} style={{backgroundColor: candyColor}} />
+          <img key={index} alt={candyColor} style={{backgroundColor: candyColor}} />
         ))}
       </div>
     </div>
