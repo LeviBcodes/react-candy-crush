2cc659bef07f8389b1bf4ffb40fcab379bd270a4
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-26 01:43:22 -0600

    creating check for rows of 3 function. set matching row of three colors to a blank string

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   const checkForColumnOfFour = () => {
     for(let i = 0; i < 39; i++) {
-      const columnOfFour = [i, i + width, i + width * 3]
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
       const decidedColor = currentColorArrangement[i]
       if(columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
         columnOfFour.forEach(square => currentColorArrangement[square] = '')
@@ -29,6 +29,17 @@ const App = () => {
       const decidedColor = currentColorArrangement[i]
       if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
         columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for(let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64]
+      if(rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
       }
     }
   }
