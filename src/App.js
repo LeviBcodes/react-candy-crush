2863d39d1548c87114883d98c3c7df9caf50cdde
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-26 01:52:34 -0600

    adding function to check for rows of 4. setting cells to empty string if  there is a row of 4 matching colors.

--- src/App.js
+++ src/App.js
@@ -33,11 +33,24 @@ const App = () => {
     }
   }
 
+  const checkForRowOfFour = () => {
+    for(let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,62,63,64]
+      if(notValid.includes(i)) continue
+      if(rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const checkForRowOfThree = () => {
     for(let i = 0; i < 64; i++) {
       const rowOfThree = [i, i + 1, i + 2]
       const decidedColor = currentColorArrangement[i]
       const notValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64]
+      if(notValid.includes(i)) continue
       if(rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
         rowOfThree.forEach(square => currentColorArrangement[square] = '')
       }
@@ -59,10 +72,12 @@ const App = () => {
     const timer = setInterval(() => {
       checkForColumnOfFour()
       checkForColumnOfThree()
+      checkForRowOfThree()
+      checkForRowOfFour()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return() => clearInterval(timer)
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForColumnOfThree,checkForRowOfThree,checkForRowOfFour, currentColorArrangement])
   return (
     <div className="app">
       <div className="game">
