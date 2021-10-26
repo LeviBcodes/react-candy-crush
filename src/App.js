43473bd949d99fe2621461faddf7dcc473974a69
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-10-26 01:15:17 -0600

    check for columns of four matching color cells

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ const candyColors = [
 const App = () => {
   const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfFour = () => {
+    for(let i = 0; i < 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+      if(columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const checkForColumnOfThree = () => {
     for(let i = 0; i < 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2]
@@ -36,11 +46,12 @@ const App = () => {
   }, [])
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour()
       checkForColumnOfThree()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return() => clearInterval(timer)
-  }, [checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
   return (
     <div className="app">
       <div className="game">
